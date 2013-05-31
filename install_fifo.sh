449d74c40b9b6959b73692b366b1eb68805522e0
Heinz N. Gies <heinz@licenser.net>
2013-05-31 17:18:15 +0300

    Update install_fifo.sh

--- install_fifo.sh
+++ install_fifo.sh
@@ -33,22 +33,22 @@ echo -n "Gateway: "
 read FIFO_GW
 echo
 echo "Branch selection:"
-echo " 1) development branch - this branch keeps track of the newest features it"
-echo "    receives basic testing but there might be breaking changes intricuded."
-echo "    In exchange all new features and advanced fixes land here within a"
-echo "    matter of hours."
-echo " 2) release branch - this branch undergoos a more careful testing before"
-echo "    it is released, it only merges the changes from dev every so often in"
-echo "    the form of a propperly numbered release. While there is no fixed"
-echo "    release cycle new releases happen around every one to two month."
+echo " rel) release branch - this branch undergoos a more careful testing before"
+echo "      it is released, it only merges the changes from dev every so often in"
+echo "      the form of a propperly numbered release. While there is no fixed"
+echo "      release cycle new releases happen around every one to two month."
+echo " dev) development branch - this branch keeps track of the newest features it"
+echo "      receives basic testing but there might be breaking changes intricuded."
+echo "      In exchange all new features and advanced fixes land here within a"
+echo "      matter of hours."
 
-echo -n "Release[1,2]: "
+echo -n "Release[dev,rel]: "
 read FIFO_BRANCH
 case $FIFO_BRANCH in
-  "1")
+  "dev")
     BRANCH=dev
     ;;
-  "2")
+  "rel")
     BRANCH=rel
     ;;
   *)
@@ -60,22 +60,68 @@ esac
 USER_SCRIPT="/opt/local/gnu/bin/echo 'http://release.project-fifo.net/pkg/${BRANCH}/' >> /opt/local/etc/pkgin/repositories.conf;\
 export PATH=/opt/local/gnu/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin;\
 export HOME=/root;\
+> /tmp/stage_1;\
 /opt/local/bin/pkgin -fy up;\
-/opt/local/bin/pkgin -y install snarl sniffle howl wiggle jingles;\
+> /tmp/stage_2;\
+/opt/local/bin/pkgin -y install snarl;\
+> /tmp/stage_3;\
+/opt/local/bin/pkgin -y install sniffle;\
+> /tmp/stage_4;\
+/opt/local/bin/pkgin -y install howl;\
+> /tmp/stage_5;\
+/opt/local/bin/pkgin -y install wiggle;\
+> /tmp/stage_6;\
+/opt/local/bin/pkgin -y install jingles;\
+> /tmp/stage_7;\
 /opt/local/gnu/bin/cp /opt/local/jingles/config/nginx.conf /opt/local/etc/nginx/nginx.conf;\
-/usr/sbin/svcadm enable epmd snarl sniffle howl wiggle nginx;\
-/opt/local/gnu/bin/sleep 10;\
+> /tmp/stage_8;\
+/usr/sbin/svcadm enable epmd;\
+> /tmp/stage_9;\
+/usr/sbin/svcadm enable wiggle;\
+while ! /opt/local/wiggle/bin/wiggle ping>/dev/null; do echo -n .; done;\
+> /tmp/stage_10;\
+/usr/sbin/svcadm enable howl;\
+while ! /opt/local/howl/bin/howl ping>/dev/null; do echo -n .; done;\
+> /tmp/stage_11;\
+/usr/sbin/svcadm enable sniffle;\
+while ! /opt/local/sniffle/bin/sniffle ping>/dev/null; do echo -n .; done;\
+> /tmp/stage_12;\
+/usr/sbin/svcadm enable snarl;\
+while ! /opt/local/snarl/bin/snarl ping>/dev/null; do echo -n .; done;\
+> /tmp/stage_13;\
+/usr/sbin/svcadm enable nginx;\
+/opt/local/gnu/bin/sleep 1;\
+> /tmp/stage_14;\
 /opt/local/sbin/fifoadm users add admin;\
+> /tmp/stage_15;\
 /opt/local/sbin/fifoadm users grant admin ...;\
+> /tmp/stage_16;\
 /opt/local/sbin/fifoadm users passwd admin admin;\
+> /tmp/stage_17;\
 /opt/local/sbin/fifoadm dtrace import /opt/local/sniffle/share/dtrace/erl_fun.json;\
+> /tmp/stage_18;\
 /opt/local/sbin/fifoadm dtrace import /opt/local/sniffle/share/dtrace/syscalls.json;\
+> /tmp/stage_19;\
 /opt/local/sbin/fifoadm dtrace import /opt/local/sniffle/share/dtrace/zfs_read.json;\
-/opt/local/sbin/fifoadm dtrace import /opt/local/sniffle/share/dtrace/zfs_write.json"
+> /tmp/stage_20;\
+/opt/local/sbin/fifoadm dtrace import /opt/local/sniffle/share/dtrace/zfs_write.json;\
+> /tmp/stage_21"
 
+
+echo "Installing chunter service."
+curl -O http://release.project-fifo.net/chunter/${BRANCH}/chunter-latest.gz > /dev/null
+gunzip chunter-latest.gz
+sh chunter-latest
+cd $PWD
+
+echo "The remaining process is automated."
+echo
+echo "Installing the dataset."
 imgadm update
 imgadm import fdea06b0-3f24-11e2-ac50-0b645575ce9d
 
+echo
+echo "Preparing the zone."
 cat <<EOF | vmadm create
 {
   "autoboot": true,
@@ -106,11 +152,56 @@ cat <<EOF | vmadm create
 }
 EOF
 
-cd /opt
-curl -O http://release.project-fifo.net/chunter/${BRANCH}/chunter-latest.gz
-gunzip chunter-latest.gz
-sh chunter-latest
-cd $PWD
+UUID=`vmadm list -p -o uuid`
+
+function percent_bar() {
+    sleep 1
+    echo -ne '\b\\'
+    sleep 1
+    echo -ne '\b|'
+    sleep 1
+    echo -ne '\b/'
+    sleep 1
+    echo -ne '\b-'
+}
+
+function wait_for_file () {
+  while [ ! -f /zones/$UUID/root/tmp/$1 ]
+  do
+    percent_bar
+  done
+  echo -ne '\b\b\b\b\b\b\b=> '
+  echo -n ${2}% -
+
+}
+
+echo "Finalizing zone setup."
+
+echo -n [> 00% -
+wait_for_file stage_1 05
+wait_for_file stage_2 10
+wait_for_file stage_3 15
+wait_for_file stage_4 20
+wait_for_file stage_5 25
+wait_for_file stage_6 30
+wait_for_file stage_7 35
+wait_for_file stage_8 40
+wait_for_file stage_9 45
+wait_for_file stage_10 50
+wait_for_file stage_11 55
+wait_for_file stage_12 60
+wait_for_file stage_13 65
+wait_for_file stage_14 70
+wait_for_file stage_15 75
+wait_for_file stage_16 80
+wait_for_file stage_17 85
+wait_for_file stage_18 90
+wait_for_file stage_19 93
+wait_for_file stage_20 98
+wait_for_file stage_21 100
+echo -en '\b'
+echo 'done.'
+
 echo "================================================================================"
 echo "The installation and initalisation might take a while, after that you can access"
 echo "your installation at:
